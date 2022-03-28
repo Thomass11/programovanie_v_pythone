5042f886615219b007bde1e9e291d81e1e06c615
Thomass11 <35189587+Thomass11@users.noreply.github.com>
2022-03-28 14:43:50 +0200

    Add files via upload
    Nahratý súbor files_taks.py

---
+++ files_task.py
@@ -0,0 +1,15 @@
+# Pripravte si 3 ľubovoľné .txt súbory, do ktorých uložíte obsah troch webových stránok,
+# napríklad z Wikipedia (EN verzia).
+# Vytvorte program, ktorý nájde počet výskytu slova v súboroch,
+# ktoré zadá používateľ cez konzolu.
+# Počet výskytu hľadaného slova v každom súbore vypíšte na konzolu.
+
+# Vstupné súbory spolu s aplikáciou nahrajte do Onedrive (formát ZIP) 
+# a zdieľajte s tomas.jalc@spsehalova.sk
+# Podpíšte sa do zdrojového súboru
+
+
+# Príklad výstupu:
+# Pocet vyskytu hladaneho slova v subore file1.txt: 17
+# Pocet vyskytu hladaneho slova v subore file2.txt: 20
+# Pocet vyskytu hladaneho slova v subore file3.txt: 3
